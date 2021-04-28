77302723d2e31b9bf410c7539ffcdd3e802f3b69
Zeekerz <pramote.p.on@gmail.com>
2021-04-28 16:55:30 +0700

    Shipping address Screen and Check out Steps

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import data from "../data.js";
 import User from "../models/userModel.js";
 import { generateToken } from "../utils.js";
@@ -16,21 +16,43 @@ userRouter.get(
   })
 );
 
-userRouter.post('/signin', expressAsyncHandler(async(req, res) => {
-  const user = await User.findOne({email: req.body.email});
-  if (user) {
-    if (bcrypt.compareSync(req.body.password, user.password)) {
-      res.send({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user)
-      });
-      return;
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
     }
-  }
-  res.status(401).send({message: 'Invalid email or password'});
-}));
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
+  })
+);
 
 export default userRouter;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,36 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
-import { productDetailsReducer, productListReducers } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducers,
+} from "./reducers/productReducers";
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from "./reducers/userReducers";
 const initialState = {
-  cart:{
-    cartItems : localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')): []
-  }
+  userSignin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : null,
+  },
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+  },
 };
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducer,
-  cart: cartReducer
-})
+  cart: cartReducer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
+});
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
